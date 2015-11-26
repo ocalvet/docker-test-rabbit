4af62d7680cb3619fcf36863e1d422e2598bf02e
Ovidio R. Calvet <calveto@gmail.com>
2015-11-26 15:23:35 -0500

    Sent message from user service and received it in reward service

--- reward-service/index.js
+++ reward-service/index.js
@@ -3,11 +3,31 @@ var connect = amqp.connect("amqp://rabbit", {
   noDelay: true
 });
 
+var channel;
+var ex = 'logs';
+
 connect.then(function (conn) {
-  console.log('Reward service connected to rabbitMQ');
-}, function(err) {
-  console.error("Error: ", err);
-});
+    console.log('Reward service connected to rabbitMQ');
+    return conn.createChannel();
+  }, function(err) {
+    console.error("Error: ", err);
+  })
+  .then(function(ch) {
+    
+    channel = ch;
+
+    ch.assertExchange(ex, 'fanout', {durable: false});
+  
+    return ch.assertQueue('', {exclusive: true});
+  })
+  .then(function(q) {
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+    channel.bindQueue(q.queue, ex, '');
+
+    channel.consume(q.queue, function(msg) {
+      console.log(" [x] %s", msg.content.toString());
+    }, {noAck: true});
+  });
 
 console.log('Reward service up...');
 
--- user-service/index.js
+++ user-service/index.js
@@ -1,11 +1,20 @@
 var amqp = require('amqplib');
 var connect = amqp.connect("amqp://rabbit");
 
-connect.then(function (conn) {
-  console.log('User service connected to rabbitMQ');
-}, function(err) {
-  console.error("Error: ", err);
-});
+connect
+  .then(function (conn) {
+    console.log('User service connected to rabbitMQ');
+    return conn.createChannel();
+  }, function(err) {
+    console.error("Error: ", err);
+  })
+  .then(function(ch) {
+      var ex = 'logs';
+      var msg = process.argv.slice(2).join(' ') || 'Hello World!';
+      ch.assertExchange(ex, 'fanout', {durable: false});
+      ch.publish(ex, '', new Buffer(msg));
+      console.log(" [x] Sent %s", msg);
+  });
 
 console.log('User service up...');
 
